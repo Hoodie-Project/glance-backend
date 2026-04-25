129004cdbf99f745d3d15bd5d273b0c0002428d1
inyong <iysh321@gmail.com>
2026-04-25 15:37:59 +0900

    Resolve region via Naver geocoding API in mock data initializer

--- src/main/java/com/hoodiev/glance/common/DataInitializer.java
+++ src/main/java/com/hoodiev/glance/common/DataInitializer.java
@@ -4,6 +4,10 @@ import com.hoodiev.glance.comment.Comment;
 import com.hoodiev.glance.comment.CommentLike;
 import com.hoodiev.glance.comment.CommentLikeRepository;
 import com.hoodiev.glance.comment.CommentRepository;
+import com.hoodiev.glance.region.GeocodingService;
+import com.hoodiev.glance.region.LocationInfo;
+import com.hoodiev.glance.region.Region;
+import com.hoodiev.glance.region.RegionRepository;
 import com.hoodiev.glance.thread.AnimalLook;
 import com.hoodiev.glance.thread.Gender;
 import com.hoodiev.glance.thread.Thread;
@@ -29,6 +33,8 @@ public class DataInitializer implements CommandLineRunner {
     private final ThreadLikeRepository threadLikeRepository;
     private final CommentRepository commentRepository;
     private final CommentLikeRepository commentLikeRepository;
+    private final RegionRepository regionRepository;
+    private final GeocodingService geocodingService;
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
@@ -41,66 +47,61 @@ public class DataInitializer implements CommandLineRunner {
                 .nickname("산책러버")
                 .title("홍대 걷다가 진짜 예쁜 분 봤어요")
                 .content("오후 3시쯤 홍대 걷고있었는데 흰색 린넨 셔츠에 청바지 입으신 분 지나가셨어요. 키 크고 얼굴도 너무 예쁘셨는데 혹시 아시는 분?")
-                .latitude(37.5563)
-                .longitude(126.9236)
+                .latitude(37.5563).longitude(126.9236)
+                .region(resolveRegion(37.5563, 126.9236))
                 .gender(Gender.FEMALE)
                 .tags(List.of("홍대", "린넨셔츠", "오후산책"))
                 .animalLooks(Set.of(AnimalLook.FOX, AnimalLook.CAT))
                 .vibeStyles(Set.of(VibeStyle.COLD_BEAUTY))
-                .password(pw)
-                .build());
+                .password(pw).build());
 
         Thread t2 = threadRepository.save(Thread.builder()
                 .nickname("카페탐방러")
                 .title("성수 카페에서 노트북 하시던 분")
                 .content("성수 어느 카페 2층 창가 자리에서 노트북 하고 계셨는데 안경 쓰고 검정 후디 입고 계셨어요. 엄청 집중해서 일하시는 모습이 멋있었습니다")
-                .latitude(37.5448)
-                .longitude(127.0557)
+                .latitude(37.5448).longitude(127.0557)
+                .region(resolveRegion(37.5448, 127.0557))
                 .gender(Gender.MALE)
                 .tags(List.of("성수", "카페", "노트북"))
                 .animalLooks(Set.of(AnimalLook.DOG))
                 .vibeStyles(Set.of(VibeStyle.WARM_HANDSOME))
-                .password(pw)
-                .build());
+                .password(pw).build());
 
         Thread t3 = threadRepository.save(Thread.builder()
                 .nickname("지하철킬러")
                 .title("2호선에서 책 읽던 분 너무 설레요")
                 .content("강남역에서 탔는데 홍대입구까지 계속 소설책 읽으시더라고요. 베이지색 코트에 짧은 단발. 내리실 때 살짝 웃으셨는데 심장이...")
-                .latitude(37.4979)
-                .longitude(127.0276)
+                .latitude(37.4979).longitude(127.0276)
+                .region(resolveRegion(37.4979, 127.0276))
                 .gender(Gender.FEMALE)
                 .tags(List.of("2호선", "강남", "독서"))
                 .animalLooks(Set.of(AnimalLook.RABBIT, AnimalLook.DEER))
                 .vibeStyles(Set.of(VibeStyle.HARMLESS, VibeStyle.FRESH))
-                .password(pw)
-                .build());
+                .password(pw).build());
 
         Thread t4 = threadRepository.save(Thread.builder()
                 .nickname("뚝섬러")
                 .title("뚝섬 한강공원 러닝하시던 분")
                 .content("저녁 7시쯤 뚝섬 한강에서 파란 러닝복 입고 뛰시던 분 계셨어요. 키 엄청 크시고 달리는 폼도 너무 멋있었는데 매일 오시나요?")
-                .latitude(37.5311)
-                .longitude(127.0674)
+                .latitude(37.5311).longitude(127.0674)
+                .region(resolveRegion(37.5311, 127.0674))
                 .gender(Gender.MALE)
                 .tags(List.of("한강", "뚝섬", "러닝"))
                 .animalLooks(Set.of(AnimalLook.WOLF))
                 .vibeStyles(Set.of(VibeStyle.COLD_HANDSOME, VibeStyle.CLASSIC_HANDSOME))
-                .password(pw)
-                .build());
+                .password(pw).build());
 
         Thread t5 = threadRepository.save(Thread.builder()
                 .nickname("망원시장단골")
                 .title("망원시장 떡볶이집 앞에서 봤어요")
                 .content("주말 낮에 망원시장 떡볶이 먹으면서 친구랑 웃으시던 분. 빨간 니트에 흰 운동화. 웃는 모습이 너무 귀여우셨어요 ㅠㅠ")
-                .latitude(37.5551)
-                .longitude(126.9097)
+                .latitude(37.5551).longitude(126.9097)
+                .region(resolveRegion(37.5551, 126.9097))
                 .gender(Gender.FEMALE)
                 .tags(List.of("망원", "망원시장", "주말"))
                 .animalLooks(Set.of(AnimalLook.HAMSTER, AnimalLook.RABBIT))
                 .vibeStyles(Set.of(VibeStyle.WARM_BEAUTY, VibeStyle.FRESH))
-                .password(pw)
-                .build());
+                .password(pw).build());
 
         // 게시글 좋아요
         addThreadLike(t1, "1.1.1.1"); addThreadLike(t1, "1.1.1.2"); addThreadLike(t1, "1.1.1.3");
@@ -149,5 +150,17 @@ public class DataInitializer implements CommandLineRunner {
     private void addCommentLike(Comment comment, String ip) {
         commentLikeRepository.save(CommentLike.builder().commentId(comment.getId()).ipAddress(ip).build());
         commentRepository.incrementLikeCount(comment.getId());
+    }
+
+    private Region resolveRegion(double lat, double lng) {
+        LocationInfo info = geocodingService.reverseGeocode(lat, lng);
+        if (info == null) return null;
+        return regionRepository.findByLegalCode(info.legalCode())
+                .orElseGet(() -> regionRepository.save(Region.builder()
+                        .legalCode(info.legalCode())
+                        .sido(info.sido())
+                        .sigungu(info.sigungu())
+                        .dong(info.dong())
+                        .build()));
     }
 }
